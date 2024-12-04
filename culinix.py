8a75f5e95926c559a9d0c55648aad5dba8686e10
Jennifer <jenniferrzhuu@gmail.com>
2024-12-04 15:58:01 -0800

    Updated Grammar, Completed Add and View

--- culinix.py
+++ culinix.py
@@ -7,21 +7,51 @@ class Recipe:
 
     def __init__(self):
         self.recipes = {
-
+            "PBJ Sandwich": ["bread", "peanut butter", "jelly", "bread"]
         } 
 
+        self.var = {}
+    
+    def cook(self, item):  
+        print(f"recipe {self.recipes}")
+        for name, recipe_ingredients in self.recipes.items(): 
+            if item == recipe_ingredients:
+                return name
+        return None
+
     def interpret(self, model): 
-        for c in model.actions: 
-            if c.__class__.__name__ == "Mix":
-                print(f"Mix")
+        for c in model.actions:  
+            if c.__class__.__name__ == "Mix":  
+                self.var[c.name] = c.ingredients
+                print(f"Mix {self.var}")
             elif c.__class__.__name__ == "Cook":
-                print(f"Cook")
+                print(f"...mixing ingredients together...cooking...")
+                dish = self.cook(self.var[c.item])
+                self.var[c.item] = dish
+                if dish is not None:
+                    print(f"--COOKING DONE--")
+                else:
+                    print(f"UH OH: Incorrect Ingredients. No Dish Found.")
             elif c.__class__.__name__ == "Serve":
-                print(f"Serve")
+                dish = self.var[c.item] 
+                if dish is None:
+                    print("Error: Failed to Cook")
+                else:
+                    print(f"Serving {dish}!")
             elif c.__class__.__name__ == "Add":
-                print(f"Add")
+                self.recipes[c.result.capitalize()] = c.ingredients
+                print(f"Added {c.result.capitalize()} to the Recipe Book")
+            elif c.__class__.__name__ == "Find":
+                print(f"Find")
+            elif c.__class__.__name__ == "View":
+                if c.key == "all":
+                    print(f"{self.recipes}")
+                elif self.recipes.get(c.key.capitalize()) is not None:
+                    print(f"Recipe for {c.key.capitalize()}: {self.recipes[c.key.capitalize()]}")
+                else:
+                    print(f"UH OH: No Recipe(s) Found")
             else:
-                print(f"FindIngredients")
+                print(f"Error: Incorrect Syntax")
 
 recipe = Recipe()
 recipe.interpret(culinix_model)
